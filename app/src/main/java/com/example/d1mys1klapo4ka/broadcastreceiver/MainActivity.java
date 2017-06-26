7c3ad5551947d260c99cc20a3fc3f23bdc979ed3
dimysiklapo4ka <dimysiklapo4ka@gmail.com>
2017-06-26 16:20:42 +0300

    BroadCast_and_Notification_AlarmMamager

--- app/src/main/java/com/example/d1mys1klapo4ka/broadcastreceiver/MainActivity.java
+++ app/src/main/java/com/example/d1mys1klapo4ka/broadcastreceiver/MainActivity.java
@@ -1,16 +1,46 @@
 package com.example.d1mys1klapo4ka.broadcastreceiver;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    AlarmManager am;
+    private EditText mTime;
 
-    public static final String NEW_CAT_DETECTED = "ua.dimysiklapo4ka.action.NEW_NOTIFICATION";
+    public static long time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTime = (EditText)findViewById(R.id.et_time);
     }
 
+    private void restartNotify() {
 
+        am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(MainActivity.this, MyReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0,
+                intent, 0);
+
+// Устанавливаем разовое напоминание
+        am.set(AlarmManager.RTC,System.currentTimeMillis() + time * 1000, pendingIntent);
+    }
+
+    public void onClick (View view){
+        switch (view.getId()){
+            case R.id.button3:
+                time = Integer.parseInt(mTime.getText().toString());
+
+                restartNotify();
+                break;
+        }
+    }
 }
--- app/src/main/java/com/example/d1mys1klapo4ka/broadcastreceiver/MyReceiver.java
+++ app/src/main/java/com/example/d1mys1klapo4ka/broadcastreceiver/MyReceiver.java
@@ -1,30 +1,29 @@
 package com.example.d1mys1klapo4ka.broadcastreceiver;
 
+import android.app.AlarmManager;
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.icu.util.TimeUnit;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
+import android.support.v4.util.TimeUtils;
 import android.widget.Toast;
 
+import java.sql.Time;
+
 public class MyReceiver extends BroadcastReceiver {
+
+    private NotificationManager nm;
 
     private int mId = 101;
 
     @Override
     public void onReceive(Context context, Intent intent) {
-
-        if (intent.getAction().equalsIgnoreCase("android.intent.action.ACTION_POWER_DISCONNECTED")){
-
-            notification(context, "Отключено");
-        }
-
-        if (intent.getAction().equalsIgnoreCase("android.intent.action.ACTION_POWER_CONNECTED")){
-
-            notification(context, "Подключено");
-        }
+        notification(context, "Ваууууу!!!");
     }
 
     private void notification (Context context, String contentText){
@@ -32,32 +31,29 @@ public class MyReceiver extends BroadcastReceiver {
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(context)
                         .setSmallIcon(R.mipmap.ic_launcher_round)
-                        .setContentTitle("Зарядное устройство")
-                        .setContentText(contentText);
+                        .setContentTitle("ХЗ")
+                        .setContentText(contentText)
+                .setWhen(System.currentTimeMillis());
 
         Intent resultIntent = new Intent(context, MainActivity.class);
 
-// The stack builder object will contain an artificial back stack for the
-// started Activity.
-// This ensures that navigating backward from the Activity leads out of
-// your application to the Home screen.
         TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
-// Adds the back stack for the Intent (but not the Intent itself)
         stackBuilder.addParentStack(MainActivity.class);
-// Adds the Intent that starts the Activity to the top of the stack
         stackBuilder.addNextIntent(resultIntent);
+
         PendingIntent resultPendingIntent =
                 stackBuilder.getPendingIntent(
                         0,
                         PendingIntent.FLAG_UPDATE_CURRENT
                 );
+
         mBuilder.setContentIntent(resultPendingIntent);
+
         NotificationManager mNotificationManager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-// mId allows you to update the notification later on
+
         mNotificationManager.notify(mId, mBuilder.build());
 
     }
-
 
 }
